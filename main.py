2952318b300af607922c0e0459c0f4fe103e2d8c
Xue64 <rufelleemmanuelpactol7@gmail.com>
2023-10-01 13:05:27 +0800

    graphs added

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-import streamlit as st
+# count subsequences
 
-st.markdown('# Hello Nize')
+arr = [1, 6, 1, 7, 4, 2, 1]
+
+
+def sum(array):
+    total = 0
+    for i in array:
+        total += i
+    return total
+
+
+def subsequence(goal):
+    count = 0
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr) + 1):
+            added = sum(arr[i:j])
+            if added == goal:
+                count += 1
+            elif added > goal:
+                break
+    return count
+
+
+print(subsequence(7))
